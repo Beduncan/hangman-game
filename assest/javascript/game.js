44c60695c9ade1c6fb15cc55403376614f5b724d
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2017-11-05 21:17:41 -0700

    added html/css

---
+++ assest/javascript/game.js
@@ -0,0 +1,27 @@
+  var pokemon = [
+  pickacho, 
+  squirtle, 
+  charmandder, 
+  bulbasaur,
+  treecko, 
+  torchic,
+  mudkip,
+  cyndaquil,
+  totodile,
+  chikorita,  
+  ];
+
+  var pokemon = pokemon[math.floor(math.random()*pokemon.length)];
+
+  var answerArray =[]
+  for (var i =0; i<word.length;i++){
+  	answerArray[i]= "_";
+  	console.log(answerArray)
+  }
+
+  var letterleft= pokemon.length 
+
+
+
+
+
