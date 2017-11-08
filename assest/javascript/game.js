147b76d0076a452f1c81409e060eac96541762f3
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2017-11-08 09:43:06 -0700

    working in console.log

--- assest/javascript/game.js
+++ assest/javascript/game.js
@@ -11,28 +11,67 @@
   'totodile',
   'chikorita',  
   ];
-//choose word randomly 
-
- var randomItem = pokemon[Math.floor(Math.random()*pokemon.length)];
- var choosenword = randomItem; 	
- var Underscore =[];
- 
-console.log(choosenword);
- //create underscores based on length
-var genrateunderscore = () => {
-	for(var i =0; i < choosenword.length; i++){
-		Underscore.push('_');
-	}
-	return Underscore;
+ var randomItem;
+ var Underscores = [];
+ var rightguess =[];
+ var wrongguess =[];
+ var guessesleft = 10;
+ var usersguess = [];  
+///start game 
+function startgame() {
+	// body...
 }
 
+//choose word randomly 
+randomItem = pokemon[Math.floor(Math.random()*pokemon.length)];
+//checking word choose 
+ console.log(randomItem);
+ //create underscores based on length
+	for(var i =0; i < randomItem.length; i++){
+		Underscores.push('_');
+	
+	}
+ //checking underscores 
+ console.log(Underscores.join(" "));
 
-
-
-console.log(genrateunderscore());
  
-//capture users guess  
-document.addEventListener('keypress', (event) => {
-	var keycode = event.keyCode;
-	console.log(event);
-});
+//getting keyypress 
+document.onkeyup = function(event)
+{
+	usersguess = event.key;
+	console.log(usersguess); 
+//if users guess is right 
+	if(randomItem.indexOf(usersguess) > -1) 
+	{
+	 for (var i =0; i < randomItem.length; i++)
+	 {
+	 	//if userguess is in the word fill in undersocres 
+	 	if(randomItem[i] === usersguess)
+	 	{
+	 		Underscores[i] = usersguess;
+	 		console.log(Underscores.join(' '));
+	 	}
+	 }
+	}
+//else user is worng 	
+	else{
+		wrongguess.push(usersguess);
+		console.log(wrongguess);
+	guessesleft--;
+	console.log("guess you have left" + guessesleft);
+	}
+//if you win 
+if (Underscores ==0)
+{
+	alert("you are the pokemon master")
+}
+// if you lose 
+if(guessesleft == 0)
+{
+	alert("you are not the best pokemon trainer")
+}
+}
+	
+
+
+
