b5f1bca068be2b7aa6d82e82f03dc0a37f64fd75
bryantduncan98@gmail.comgit config --global user.email bryantduncan98@gmail.com <bryantduncan98@gmail.com>
2018-01-24 17:34:32 -0700

    working product

--- assest/javascript/game.js
+++ assest/javascript/game.js
@@ -1,5 +1,4 @@
  //global vari
-
  var pokemon = [
  'pickacho', 
  'squirtle', 
@@ -12,99 +11,102 @@
  'totodile',
  'chikorita',  
  ];
- var choosenword;
  var Underscores = [];
  var rightguess =0;
  var wrongguess =[];
  var guessesleft = 11;
  var usersguess = [];  
  var winC = 0;
+var wins = 0;
+ 
 
-
-//ready function 
-
-
-
-//choose word randomly 
-choosenword = pokemon[Math.floor(Math.random()*pokemon.length)];
-	//checking word choose 
-	console.log(choosenword);
-//create underscores based on length
-for(var i =0; i < choosenword.length; i++)
-{
- 	Underscores.push('_');
-}	
-
-//getting keyypress 
-document.onkeyup = function(event)
-{
-	usersguess = event.key;
-	console.log(usersguess); 
-	//if users guess is right 
-	if(choosenword.indexOf(usersguess) > -1) 
+	//geting ids from html 
+	function display1()
 	{
-		for (var i =0; i < choosenword.length; i++)
+		document.getElementById("score").innerHTML = Underscores.join(' ');
+		document.getElementById("left").innerHTML =("you have " + guessesleft + " guesses left");
+		document.getElementById("lettersguessed").innerHTML = wrongguess;
+	}	
+
+	function start()
+	{
+		//choose word randomly 
+		choosenword = pokemon[Math.floor(Math.random()*pokemon.length)];
+			//checking word choose 
+			console.log(choosenword);
+		//create underscores based on length
+		for(var i =0; i < choosenword.length; i++)
 		{
-		 	// if userguess is in the word fill in undersocres 
-		 	if(choosenword[i] === usersguess)
-			{		
-		 	Underscores[i] = usersguess;
-		 	console.log(Underscores.join(' '));
-		 	display();
-		 	}
-		 	// if guesses is right add letter to underscores 
-		 	if (Underscores[i] === usersguess)
-		 	{
-		 		rightguess++;
-		 	}
-		}
+ 			Underscores.push('_');
+		}	
 	}
+	//calling functions
+	start();
+	display1();
 	
-			// else user is worng 	
-			else
-			{
-				wrongguess.push(usersguess);
-				console.log(wrongguess);
-				guessesleft--;
-				display();
-			}			
-			
-
-
-function display() 
-{
-	// geting elements from id 
-	document.getElementById("score").innerHTML = Underscores.join(' ');
-	document.getElementById("left").innerHTML =("you have " + guessesleft + " guesses left");
-	document.getElementById("lettersguessed").innerHTML = (wrongguess);
-}
-
-
-	
+	function click()
+	{
+		//getting keyypress 
+		document.onkeyup = function(event)
+		{
+			usersguess = event.key;
+				//if users guess is right 
+				if(choosenword.indexOf(usersguess) > -1) 
+				{
+					for (var i =0; i < choosenword.length; i++)
+					{
+			 			// if userguess is in the word fill in undersocres 
+			 			if(choosenword[i] === usersguess)
+						{		
+			 				Underscores[i] = usersguess;
+			 				console.log(Underscores.join(' '));
+			 				display();
+			 			}
+			 			// if guesses is right add letter to underscores 
+			 			if (Underscores[i] === usersguess)
+			 			{
+			 			rightguess++;
+			 			}
+					}
+				}
+		
+				// else user is worng 	
+				else
+				{
+					wrongguess.push(usersguess);
+					console.log(wrongguess);
+					guessesleft--;
+					display();
+				}			
+		//closeing onkey up
+		}			
+	}
 	//if you win	
 	if(rightguess == choosenword.length)
 	{
 		alert("You are The Pokemon Champion")	
 		winlose();	
+		
 	}
 	 		
 	//if you lose
 	if(guessesleft === 0)
 	{
-	alert("You are Not The Pokemon Champion")
-	winlose();
+		alert("You are Not The Pokemon Champion")
+		winlose();
 	}	
-function winlose()
-{
-if (guessesleft === 0)
-		{
-	    	window.location = 'https://beduncan.github.io/hangman-game/'; 
-		}
-if(rightguess == choosenword.length)
-			{
-				window.location = 'https://beduncan.github.io/hangman-game/'; 
-			}
 
-}
-//onkeyclose
-}
+	function winlose()
+	{
+		if (guessesleft === 0)
+		{
+		   	window.location = 'https://beduncan.github.io/hangman-game/'; 
+		}
+		if(rightguess === choosenword.length)
+		{
+			window.location = 'https://beduncan.github.io/hangman-game/'; 
+		}
+	}
+
+
+
